5e2bbc94814b8a39704ee5c7a46d84dff2870ebd
HajarELHAMIDY <hajar.elhamidy@gmail.com>
2021-01-23 21:38:41 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsManagementComponent } from './products-management/products-management.component';
+import { CategoryComponent } from './category/category.component';
+import { ClientComponent } from './client/client.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RegisterCategoryComponent } from './register-category/register-category.component';
+import { RegisterClientComponent } from './register-client/register-client.component';
+import { RegisterProductComponent } from './register-product/register-product.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsManagementComponent,
+    CategoryComponent,
+    ClientComponent,
+    RegisterCategoryComponent,
+    RegisterClientComponent,
+    RegisterProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule, FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
